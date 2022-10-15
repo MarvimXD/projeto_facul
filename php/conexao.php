dd808139190ee94d32b9ab2680616b6befab4a06
Victor Beserra <58988379+MarvimXD@users.noreply.github.com>
2022-10-14 22:29:18 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,8 +1,14 @@
 <?php
 
+// define('HOST', 'localhost');
+// define('USER', 'id19712670_root');
+// define('PASS', '9%*rVFjsk=2-J1G6');
+// define('DB', 'id19712670_circo');
+
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
 define('DB', 'circo');
+
 
 $conexao = mysqli_connect(HOST, USER, PASS, DB) or die("Não conectou!");
